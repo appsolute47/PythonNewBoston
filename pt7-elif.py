299d507eef0244d502eeec6d3e5ec0171cbf2a26
Paul D <androlet@icloud.com>
2018-02-14 05:24:47 +0000

    elif

---
+++ pt7-elif.py
@@ -0,0 +1,12 @@
+name = "Lucy"
+
+if name is "Bucky":
+    print("Hey there Bucky")
+
+elif name is "Lucy":
+    print("What up Lucy")
+
+elif name is "Sammy":
+    print("Whats up Sammy")
+else:
+    print("Please sign up")
