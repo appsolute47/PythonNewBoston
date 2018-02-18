c61ac4ef95e511575f41760087d792e924970657
Paul D <androlet@icloud.com>
2018-02-18 16:43:49 +0000

    modified back to original

--- pt8-for.py
+++ pt8-for.py
@@ -1,11 +1,11 @@
-#foods = ['bacon', 'tuna', 'ham', 'sausages', 'beef']
-
-#for f in foods:
-#    print(f)
-#    print(len(f))
-
 foods = ['bacon', 'tuna', 'ham', 'sausages', 'beef']
 
-for f in foods[:2]:
+for f in foods:
     print(f)
     print(len(f))
+
+#foods = ['bacon', 'tuna', 'ham', 'sausages', 'beef']
+
+#for f in foods[:2]:
+#    print(f)
+#    print(len(f))
