2f30c2c65a7d51c2eb0e60600a54d4f6cdb86c0d
Paul D <androlet@icloud.com>
2018-02-27 19:48:52 +0000

    For & While

---
+++ pt9-range-while.py
@@ -0,0 +1,18 @@
+# For loop
+
+for x in range(10):
+    print("Paul")
+
+for x in range(5, 12):
+   print(x)
+
+for x in range(10, 40, 5):
+   print(x)
+
+# While
+
+buttcrack = 5
+
+while buttcrack < 10:
+    print(buttcrack)
+    buttcrack +=1
