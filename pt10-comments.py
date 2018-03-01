65ea0ff039a0a262907b1412ce42903c79a7f296
Paul D <androlet@icloud.com>
2018-03-01 19:30:47 +0000

    comments

---
+++ pt10-comments.py
@@ -0,0 +1,21 @@
+magicNumber = 26
+
+for n in range(101):
+    if n is magicNumber:
+        print(n, "is the magic number")
+        break
+    else:
+        print(n)
+
+# Single line comment
+
+'''
+Multi line comment
+
+for n in range(101):
+    if n is magicNumber:
+        print(n, "is the magic number")
+        break
+    else:
+        print(n)
+'''
